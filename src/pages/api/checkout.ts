b059b437264d28e2f412c72a48a4c79503329204
Fernando Silva <fernandosilva.smsrio@gmail.com>
2023-04-21 22:23:11 -0300

    fix: https

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -12,8 +12,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({ error: 'Price not found.' });
   }
 
-  const successUrl = `${process.env.VERCEL_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
-  const cancelUrl = `${process.env.VERCEL_URL}/`;
+  const successUrl = `https://${process.env.VERCEL_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
+  const cancelUrl = `https://${process.env.VERCEL_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
     success_url: successUrl,
